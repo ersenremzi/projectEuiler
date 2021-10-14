9ba8602c39c154233eda501f0fc63bcb21bd42d5
ersenremzi <88109467+ersenremzi@users.noreply.github.com>
2021-10-14 14:43:43 -0200

    Update SolutionTwo.java

--- SolutionTwo.java
+++ SolutionTwo.java
@@ -1,5 +1,10 @@
 package ProjectEulerSolutions;
 
+//Fibonacci dizisindeki her yeni terim, önceki iki terimin eklenmesiyle oluşturulur. 1 ve 2 ile başlayarak, ilk 10 terim şöyle olacaktır:
+//1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+Fibonacci dizisindeki değerleri dört milyonu geçmeyen terimleri dikkate alarak çift değerli terimlerin toplamını bulunuz.
+
 public class SolutionTwo {
     public static void main(String[] args) {
         int sayi = 0;
