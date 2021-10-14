7f10437ad44d47a3e1307c31f37166b3a9e71baa
ersenremzi <88109467+ersenremzi@users.noreply.github.com>
2021-10-14 13:03:23 -0200

    Update SolutionOne.java

--- SolutionOne.java
+++ SolutionOne.java
@@ -1,6 +1,10 @@
 package ProjectEulerSolutions;
 
 public class SolutionOne {
+    
+    //10'un altında 3 veya 5'in katı olan tüm doğal sayıları listelersek 3, 5, 6 ve 9 elde ederiz. Bu katların toplamı 23'tür.
+
+    //1000'in altındaki 3 veya 5'in tüm katlarının toplamını bulun.
 
     public static void main(String[] args) {
         int sayi1 = 0;
